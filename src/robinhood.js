b14c84dc8a7566f572c5c4f2b654186c5e47bf36
TradersIQ <62960583+TradersIQ@users.noreply.github.com>
2021-10-25 23:15:03 -0700

    Update robinhood.js

--- src/robinhood.js
+++ src/robinhood.js
@@ -96,7 +96,9 @@ function RobinhoodWebApi(opts, callback) {
       Accept: '*/*',
       'Accept-Encoding': 'gzip, deflate',
       Referer: 'https://robinhood.com/',
-      Origin: 'https://robinhood.com'
+      Origin: 'https://robinhood.com',
+      'Access-Control-Allow-Origin' : 'http://localhost:3000',
+      'Access-Control-Allow-Credentials' : true,
     };
     _setHeaders();
     if (!_private.auth_token) {
